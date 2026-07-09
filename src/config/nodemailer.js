f5910221ce5bf93959b05ce0ffb82e7a6e180b5f
Ritesh Jangid <jangidritesh570@gmail.com>
2026-07-09 22:25:19 +0530

    mailer

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -3,11 +3,19 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // true only for port 465
+  secure: false, // Port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.log("SMTP VERIFY ERROR:", err);
+  } else {
+    console.log("SMTP Server Ready");
+  }
 });
 
 export default transporter;
